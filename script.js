c9a0ce81d2c49f058ae33007736dde5b7a72d436
Douglas-Bassey <godspowerbassey11@gmail.com>
2026-04-23 12:13:48 +0100

    sec

---
+++ script.js
@@ -0,0 +1,62 @@
+/* 
+INTRODUCTION  TO JAVASCRIPT
+
+JAVASCRIPT is a high level, objecct oriented multi-paradigm programming language.
+
+By high level, it means that we donot have to worry about complex stuff like memory management.
+
+By object-oriented we mean that javascript supports the use of objects which are collections of properties and methods that can represent real world entities.
+
+Multi-paradigm means that javascript supports various programming styles including procedural, objected-oriented, and functional programming giving developers the flexibility in how they structure codes.
+
+By language we are referring to a tool that allows us to communicate instructions to a computer in a way that it can understand and execute.
+
+
+JAVASCRIPT SYNTAX AND VARIABLES
+JavaScript syntax is simply the rules that define how you write valid JavaScript code—how statements are structured, how variables are declared, how functions are written, and how everything fits together so the engine understands you.
+
+
+JAVASCRIPT STATEMENT  
+A statement is a complete instruction that JavaScript executes.
+
+COMMENT
+A comment is code that JavaScript ignores completely.
+
+VARIABLES
+A variable in JavaScript is a named container used to store data so you can use and reuse it later.
+
+Value is a piece of data. it is the most fundamental unit of information that we have in programming. call it the smallest unit of information, e.g 23, 45, "food", etc
+*/
+
+var age = 90;
+// console.log(age);
+
+let fullName = "Nyetoro Ime"
+console.log(fullName, age)
+
+/* 
+
+variables naming rules and conventions:
+1. variables names should be descriptive e.g let color = "mango" is wrong. let color ="blue" is correct.
+
+2. variable names must start with (a-z, A-Z) e.g if the variable contains just a name, use lower case letter, let age = 80
+// if it contains two words (names) use:
+//a. camel case e.g const firstName = "Nyetoro"
+//b. snake case e.g let his_age = 90
+//c. pascal case e.g let MyAge = 70
+
+3. variable names cannot start with a digit
+
+4. variable names can start with $ sign e.g $name = "Hypeman"
+5. Do not use reserved keywords
+
+*/
+
+let firstJob = "teacher";
+let secondJob = "programmer";
+
+let greeting = "HELLO";
+// console.log(greeting)
+
+let firstName = "James"
+console.log(greeting, firstName)
