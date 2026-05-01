10da7b6389910d574dd386207f989290c4287715
Douglas-Bassey <godspowerbassey11@gmail.com>
2026-05-01 16:30:28 +0100

    update

--- assign.js
+++ assign.js
@@ -30,3 +30,66 @@ let graduate = true
 console.log("My name is " + studentName + " i am " + $age + " years old " + " my favourite subject is " + favourite_subject + " i am a graduate " + graduate)
 
 alert("assignment")
+
+// Assignment 2
+// 1. An array of 5 of my favourite food
+
+let favouriteFood = [
+    "coconut rice with beef and fried plantain",
+    "Banga soup",
+    "Chinese egg fried rice",
+    "Afang soup",
+    "Stew served with any edible that medically good"
+];
+console.log(favouriteFood)
+
+// 2. an object of car with 4 properties 
+
+let carProperties = {
+    suv: "engine",
+    cyberTruck: "Tesla",
+    ferrari: "Speed",
+    lambo: "rearmirror view"
+};
+console.log(carProperties)
+
+// 2a. log each to console
+console.log(carProperties.suv);
+console.log(carProperties.cyberTruck);
+console.log(carProperties.ferrari);
+console.log(carProperties.lambo);
+
+// 3. create 5 variables of different data types
+var first = "Destiny";
+var second = 20;
+var third = true;
+var java;
+var sure = "";
+console.log(first, second, third, java, sure)
+
+// 4. myName variable and log to console
+const myName = "God's power Douglas Bassey";
+console.log(myName) 
+
+// 5. arithemtic operation on 5 variables
+let f1 = 2 + 2;
+let f2 = 2 * 2;
+let f3 = 2 - 2;
+let f4 = 4 / 2;
+let f5 = 5 % 2;
+console.log(f1, f2, f3, f4, f5)
+
+// 6. variable for score and performing assignment operations
+let score = 10;
+score += 10;
+console.log(score);
+score *= 2;
+console.log(score);
+score -= 20;
+console.log(score);
+score /= 20;
+console.log(score);
+score %= 7;
+console.log(score);
+score **= 2;
+console.log(score)
